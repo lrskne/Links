50df03342beb5c6a80aca952b373768715b21116
Laura Erskine <lrskne@gmail.com>
2013-07-15 07:33:48 -0700

    Inital commit, scaffold not done yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpfulLinks::Application.config.secret_token = 'dc12b4a13cf627f2e7eeabe3d849b60fa28f56dd22adef077fa32bb59b9e885b36ed29019cefb8c22544f034bf20dcda74aa3d697202c48c76a867ac79034c00'
